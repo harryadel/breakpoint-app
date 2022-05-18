9c89b593a82c6b0cfd9553cd0a29bfadc7bfc74a
harryadel <harryadelb@gmail.com>
2022-05-18 11:55:16 +0200

    Init project

---
+++ server/main.js
@@ -0,0 +1,34 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { LinksCollection } from '/imports/api/links';
+
+function insertLink({ title, url }) {
+  LinksCollection.insert({title, url, createdAt: new Date()});
+}
+
+Meteor.startup(() => {
+  check("123", Number);
+
+  // If the Links collection is empty, add some data.
+  if (LinksCollection.find().count() === 0) {
+    insertLink({
+      title: 'Do the Tutorial',
+      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+    });
+
+    insertLink({
+      title: 'Follow the Guide',
+      url: 'http://guide.meteor.com'
+    });
+
+    insertLink({
+      title: 'Read the Docs',
+      url: 'https://docs.meteor.com'
+    });
+
+    insertLink({
+      title: 'Discussions',
+      url: 'https://forums.meteor.com'
+    });
+  }
+});
